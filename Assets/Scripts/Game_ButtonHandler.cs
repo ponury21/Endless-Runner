03b4ac0a06bb8597d4918469aa8b0e10a9d4da42
ponury21 <ponury21@gmail.com>
2019-11-14 14:00:35 +0100

    First commit

---
+++ Assets/Scripts/Game_ButtonHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game_ButtonHandler : MonoBehaviour
+{
+    public void backToMenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}

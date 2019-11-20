0c537fc773817a5bcbbbf49578f7127ae4212cb0
lenekk <dominiklenko@gmail.com>
2019-11-20 18:22:20 +0100

    wstep

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public bool isGrounded=true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rigidbody = transform.GetComponent<Rigidbody>();
+    }
+ 
+    void onCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag==("Ground")&& isGrounded == false)
+        {
+            isGrounded = true;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody>().AddTorque(Vector3.left*50f);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody>().AddTorque(Vector3.back*50f);
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody>().AddTorque(Vector3.forward*50f);
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Rigidbody>().AddTorque(-Vector3.left*50f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)&& isGrounded)
+        {
+            
+             GetComponent<Rigidbody>().AddForce(0, 0.7f, 0, ForceMode.Impulse);
+            isGrounded = false;
+        }
+      
+        
+    }
+}
---
+++ Assets/kamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kamera : MonoBehaviour
+{
+
+    public Transform sfera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 wektor = new Vector3(0, 20, 100);
+        Rigidbody rigidbody = sfera.GetComponent<Rigidbody>();
+        float velocity = rigidbody.velocity.sqrMagnitude;
+        wektor = wektor * (1 + velocity / 100);
+        transform.position = Vector3.Lerp(sfera.position, sfera.position + wektor,0.1f);
+        transform.LookAt(sfera);
+    }
+}

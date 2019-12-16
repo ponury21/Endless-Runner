8e70a4aa1a3e80e61283993379b7c842091b0403
tiagon <radek.wojciechowski1997@gmail.com>
2019-12-16 20:29:23 +0100

    Dodanie liczenia punktów

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public Transform player;
+    public Text scoreText;
+    public float currentScore;
+
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        currentScore = Mathf.Abs(player.position.z - 104);
+        scoreText.text =  currentScore.ToString("0");
+    }
+
+    
+}

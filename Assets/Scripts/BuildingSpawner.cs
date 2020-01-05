c08db3f595275fac14a04ae13b65edd29860bf83
ponury21 <ponury21@gmail.com>
2020-01-05 19:08:38 +0100

    Dodanie budynków i spawn budynków, dodanie postaci

---
+++ Assets/Scripts/BuildingSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSpawner : MonoBehaviour
+{
+    public GameObject[] Building;
+    public Transform[] BuildingSpawnPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0;i<BuildingSpawnPoints.Length;i++){
+            CreateObject(BuildingSpawnPoints[i].position, Building[Random.Range(0, Building.Length)]);
+        }
+    }
+
+    void CreateObject(Vector3 position, GameObject prefab)
+    {
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+}
--- Assets/Scripts/StuffSpawner.cs
+++ Assets/Scripts/StuffSpawner.cs
@@ -10,20 +10,13 @@ public class StuffSpawner : MonoBehaviour
 
     public GameObject[] Obstacle;
     public float minX = -2f, maxX = 2f;
-    // Start is called before the first frame update
     void Start()
     {
-       // bool placeObstacle=Random.Range(0,2)==0;
-       // int obstacleIndex=-1;
-       // if(placeObstacle){
-       //     obstacleIndex=Random.Range(1,StuffSpawnPoints.Length);
-       //     CreateObject(StuffSpawnPoints[obstacleIndex].position,Obstacle[Random.Range(0,Obstacle.Length)]);
-       // }
+       
 
         for (int i = 0; i < StuffSpawnPoints.Length; i++)
         {
-           // if (i == obstacleIndex) continue;
-            if (Random.Range(0, 2) == 0) //33% chances to create candy
+            if (Random.Range(0, 2) == 0) 
             {
                 CreateObject(StuffSpawnPoints[i].position, Bonus[Random.Range(0, Bonus.Length)]);
             }
